79ea4c481d641cd7bf7d9f89c9942be9b57ab3ba
Ziyan Zhou <zhou@ziyan.net>
2019-12-07 21:13:14 +0900

    Some progress on implementation.

--- cli/cli.go
+++ cli/cli.go
@@ -1,24 +1,19 @@
 package cli
 
 import (
+	"context"
 	"os"
+	"os/exec"
+	"syscall"
+	"time"
 
 	"github.com/op/go-logging"
 	"github.com/urfave/cli"
+
+	"github.com/ziyan/panto/utils"
 )
 
 var log = logging.MustGetLogger("cli")
-
-func configureLogging(level, format string) {
-	logging.SetBackend(logging.NewBackendFormatter(
-		logging.NewLogBackend(os.Stderr, "", 0),
-		logging.MustStringFormatter(format),
-	))
-	if level, err := logging.LogLevel(level); err == nil {
-		logging.SetLevel(level, "")
-	}
-	log.Debugf("log level set to %s", logging.GetLevel(""))
-}
 
 func Run(args []string) {
 
@@ -29,21 +24,88 @@ func Run(args []string) {
 	app.Usage = "A tool to collate system log and events."
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "log-level",
 			Value: "INFO",
 			Usage: "log level",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "log-format",
 			Value: "%{color}%{time:2006-01-02T15:04:05.000Z07:00} [%{level:.4s}] [%{shortfile} %{shortfunc}] %{message}%{color:reset}",
 			Usage: "log format",
 		},
+		&cli.StringFlag{
+			Name:  "remote-executable-path",
+			Value: "/tmp/panto",
+		},
+		&cli.StringSliceFlag{
+			Name:  "remote",
+			Usage: "remote hostname",
+		},
+	}
+
+	app.Before = func(c *cli.Context) error {
+		logging.SetBackend(logging.NewBackendFormatter(
+			logging.NewLogBackend(os.Stderr, "", 0),
+			logging.MustStringFormatter(c.String("log-format")),
+		))
+		if level, err := logging.LogLevel(c.String("log-level")); err == nil {
+			logging.SetLevel(level, "")
+		}
+		log.Debugf("log level set to %s", logging.GetLevel(""))
+		log.Debugf("hostname is %s", utils.Hostname)
+		return nil
+	}
+
+	app.Commands = []*cli.Command{
+		&cli.Command{
+			Name: "server",
+			Action: func(c *cli.Context) error {
+				time.Sleep(60 * time.Second)
+				return nil
+			},
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		configureLogging(c.String("log-level"), c.String("log-format"))
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
+		for _, remote := range c.StringSlice("remote") {
+			if err := utils.DeployExecutable(ctx, remote, c.String("remote-executable-path")); err != nil {
+				return err
+			}
+		}
+
+		var cmds []*exec.Cmd
+		defer func() {
+			cancel()
+			for _, cmd := range cmds {
+				if err := cmd.Wait(); err != nil {
+					log.Errorf("command %s exited with: %s", cmd, err)
+				}
+			}
+		}()
+
+		for _, remote := range c.StringSlice("remote") {
+			cmd, _, err := utils.RunRemoteExecutable(ctx, remote, c.String("remote-executable-path"), "--log-level", c.String("log-level"), "server")
+			if err != nil {
+				return err
+			}
+			cmds = append(cmds, cmd)
+		}
+
+		time.Sleep(2 * time.Second)
+
+		for _, cmd := range cmds {
+			if cmd.Process != nil {
+				if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+					log.Errorf("failed to signal command %s: %s", cmd, err)
+				}
+			}
+		}
+
+		time.Sleep(1 * time.Second)
 		return nil
 	}
 
---
+++ utils/utils.go
@@ -0,0 +1,81 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("utils")
+
+var Hostname = func() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return hostname
+}()
+
+var Executable = func() string {
+	executable, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	return executable
+}()
+
+func DeployExecutable(ctx context.Context, remote, path string) error {
+	return exec.CommandContext(ctx, "rsync", "--perms", "--times", "--compress", "--checksum", Executable, fmt.Sprintf("%s:%s", remote, path)).Run()
+}
+
+func RunRemoteExecutable(ctx context.Context, remote, path string, args ...string) (*exec.Cmd, io.ReadWriteCloser, error) {
+	cmd := exec.CommandContext(ctx, "ssh", append([]string{remote, path}, args...)...)
+	cmd.Stderr = os.Stderr
+
+	wc, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func() {
+		if wc != nil {
+			wc.Close()
+		}
+	}()
+
+	rc, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func() {
+		if rc != nil {
+			rc.Close()
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		return nil, nil, err
+	}
+
+	rwc := &readWriteCloser{ReadCloser: rc, WriteCloser: wc}
+	rc = nil
+	wc = nil
+	return cmd, rwc, nil
+}
+
+type readWriteCloser struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (rwc *readWriteCloser) Close() error {
+	err := rwc.WriteCloser.Close()
+	err2 := rwc.ReadCloser.Close()
+	if err == nil {
+		err = err2
+	}
+	return err
+}
